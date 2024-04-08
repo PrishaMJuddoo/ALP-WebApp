abf8ad57f4fa48610ae09f1bade5ad67bd93377b
Shyam Prasad <xshyamprasadx@yahoo.com.sg>
2024-04-08 17:47:25 +0800

    small changes

--- create-app/src/pages/Login.js
+++ create-app/src/pages/Login.js
@@ -56,49 +56,52 @@ function Login() {
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-lg-6">
-            <div className="card o-hidden border-0 shadow-lg my-5">
-              <div className="card-body p-0">
-                <div className="p-5">
-                  <div className="text-center">
-                    <h1 className="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                  </div>
-                  <form className="user" onSubmit={handleSubmit}>
-                    {/* Username input */}
-                    <div className="form-group">
-                      <input
-                        type="text"
-                        className="form-control form-control-user"
-                        value={username}
-                        onChange={handleChangeUsername}
-                        placeholder="Enter Username"
-                      />
-                    </div>
-                    {/* Password input */}
-                    <div className="form-group">
-                      <input
-                        type="password"
-                        className="form-control form-control-user"
-                        value={password}
-                        onChange={handleChangePassword}
-                        placeholder="Enter Password"
-                      />
-                    </div>
-                    {/* Submit button */}
-                    <button type="submit" className="btn btn-primary btn-user btn-block">
-                      Login
-                    </button>
-                  </form>
-                  {/* Display login message */}
-                  <p>{loginMessage}</p>
-                  <hr />
-                  <div className="text-center">
-                    <a className="small" href="forgot-password.html">
-                      Forgot Password?
-                    </a>
-                  </div>
+            {/* <div className="card o-hidden border-0 shadow-lg my-5"> */}
+            {/* <div className="card-body p-0"> */}
+            <div className="p-5">
+              <div className="text-center">
+                <h1 className="h4 text-gray-900 mb-4">Welcome Back!</h1>
+              </div>
+              <form className="user" onSubmit={handleSubmit}>
+                {/* Username input */}
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-user"
+                    value={username}
+                    onChange={handleChangeUsername}
+                    placeholder="Enter Username"
+                  />
                 </div>
+                {/* Password input */}
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-user"
+                    value={password}
+                    onChange={handleChangePassword}
+                    placeholder="Enter Password"
+                  />
+                </div>
+                {/* Submit button */}
+                <button
+                  type="submit"
+                  className="btn btn-primary btn-user btn-block"
+                >
+                  Login
+                </button>
+              </form>
+              {/* Display login message */}
+              <p>{loginMessage}</p>
+              <hr />
+              <div className="text-center">
+                <a className="small" href="forgot-password.html">
+                  Forgot Password?
+                </a>
               </div>
             </div>
+            {/* </div> */}
+            {/* </div> */}
           </div>
         </div>
       </div>
